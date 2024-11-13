69a86a6292d0c517448c97f217c138c0a9ed1d51
Matteu99 <mateusz.wawrzyniak.1@gmail.com>
2024-11-13 21:00:56 +0100

    zejsciowka3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,28 @@
 #pragma once
-
 #include "Stocznia.hpp"
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int przetransportowano = 0;
+    unsigned int liczbaZaglowcow = 0;
+    Stocznia stocznia;
+
+    while (przetransportowano < towar) {
+        // Tworzymy statek przy pomocy fabryki
+        Statek* statek = stocznia();
+
+        // Transportujemy towar przy pomocy statku
+        unsigned int iloscPrzetransportowana = statek->transportuj();
+        przetransportowano += iloscPrzetransportowana;
+
+        // Sprawdzamy, czy stworzony statek to Zaglowiec
+        if (dynamic_cast<Zaglowiec*>(statek)) {
+            liczbaZaglowcow++;
+        }
+
+        // Niszczenie stworzonego statku
+        delete statek;
+    }
+
+    return liczbaZaglowcow;
 }
